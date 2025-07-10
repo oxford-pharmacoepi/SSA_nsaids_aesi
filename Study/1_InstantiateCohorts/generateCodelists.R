323e0e11762b3dda4744fb82149387399e608207
Elin Rowlands <elinrow@gmail.com>
2025-07-10 10:55:13 +0100

    Add codelists.

---
+++ Study/1_InstantiateCohorts/generateCodelists.R
@@ -0,0 +1,151 @@
+ace_inhib <- getATCCodes(
+  cdm,
+  level = c("ATC 3rd"),
+  name = "ACE INHIBITORS, PLAIN",
+  doseForm = NULL,
+  doseUnit = NULL,
+  routeCategory = NULL,
+  type = "codelist"
+)
+
+cough_codes <- getDescendants(cdm, conceptId = c(254761))
+
+# use codelists generator to get the ingredient levels for all nsaids -----
+# extract ATC for NSAIDS at 4th level to get the ingredients
+
+# M01AA Butylpyrazolidines
+# M01AB Acetic acid derivatives and related substances
+# M01AC Oxicams
+# M01AE Propionic acid derivatives
+# M01AG Fenamates
+# M01AH Coxibs
+# M01AX other anti inflammatories
+
+nsaids_lists <- getATCCodes(
+  cdm,
+  level = c("ATC 4th"),
+  name = c("Butylpyrazolidines",
+           "Acetic acid derivatives and related substances",
+           "Oxicams",
+           "Propionic acid derivatives",
+           "Fenamates",
+           "Coxibs",
+           "Other antiinflammatory and antirheumatic agents, non-steroids"),
+  doseForm = NULL,
+  doseUnit = NULL,
+  routeCategory = NULL,
+  type = "codelist_with_details"
+)
+
+# collapse the elements of the lists
+nsaids_lists_ingredients <- nsaids_lists %>% 
+  data.table::rbindlist()
+
+# get the ingredients from the list by binding with concept table
+nsaids_lists_ingredients <- cdm$concept %>% filter(concept_id %in% nsaids_lists_ingredients$concept_id) %>% 
+  filter(concept_class_id == "Ingredient") %>% 
+  collect()
+
+# filter out the ones which are not nsaids
+# exclusions these are ingredients give in combination which are not nsaids
+exclusions <- c("methocarbamol",
+                "carisoprodol" ,
+                "magaldrate",
+                "magnesium carbonate",
+                "aluminum hydroxide",
+                "betamethasone",
+                "dexpanthenol",
+                "loperamide",
+                "misoprostol",
+                "codeine",
+                "vitamin B12",
+                "dexamethasone",
+                "amoxicillin",
+                "ampicillin",
+                "pridinol",
+                "cholestyramine resin",
+                "magnesium",
+                "thiamine",
+                "pyridoxine",
+                "avocado oil",
+                "polysulfated glycosaminoglycan",
+                "avocado soybean unsaponifiables",
+                "diacetylrhein",
+                "orgotein",
+                "soybean oil",
+                "bumadizone",
+                "benzydamine",
+                "oxaceprol",
+                "bufexamac",
+                "chondroitin sulfates",
+                "glucosamine")
+
+# remove the exclusions from the list of nsaid ingredients
+nsaids_lists_ingredients <- nsaids_lists_ingredients %>% 
+  filter(!(concept_name %in% exclusions))
+
+
+# other NSAIDs are caputured elsewhere in ATC classification:
+nsaids_lists1 <- getATCCodes(
+  cdm,
+  level = c("ATC 4th"),
+  name = c("Salicylic acid and derivatives",
+           "Pyrazolones",
+           "Other analgesics and antipyretics"),
+  doseForm = NULL,
+  doseUnit = NULL,
+  routeCategory = NULL,
+  type = "codelist_with_details"
+)
+
+nsaids_lists_ingredients1 <- nsaids_lists1 %>% 
+  data.table::rbindlist()
+
+# get the ingredients from the list by binding with concept table
+nsaids_lists_ingredients1 <- cdm$concept %>% filter(concept_id %in% nsaids_lists_ingredients1$concept_id) %>% 
+  filter(concept_class_id == "Ingredient") %>% 
+  collect()
+
+inclusions <- c("methyl salicylate",
+                "salicylic acid" ,
+                "diflunisal",
+                "salsalate",
+                "aspirin",
+                "salicylamide",
+                "magnesium salicylate",
+                "morpholine salicylate",
+                "Guacetisal",
+                "Carbasalate Calcium",
+                "Salacetamide",
+                "aloxiprin",
+                "ethenzamide",
+                "dipyrocetyl" ,
+                "phenazone",
+                "propyphenazone",
+                "aminophenazone",
+                "nifenazone",
+                "floctafenine")
+
+# keep the inclusions from the list of nsaid ingredients
+nsaids_lists_ingredients1 <- nsaids_lists_ingredients1 %>% 
+  filter(concept_name %in% inclusions)
+
+
+# bind rows to get final list
+nsaids_lists_ingredients <- bind_rows(nsaids_lists_ingredients,
+                                      nsaids_lists_ingredients1)
+
+# put the nsaids list into codelist generator and do more filtering of concepts
+nsaids_codelist1 <- getDrugIngredientCodes(
+  cdm,
+  name = nsaids_lists_ingredients$concept_name,
+  nameStyle = "{concept_name}",
+  routeCategory = "oral",
+  ingredientRange = c(1, 1),
+  type = "codelist")
+  
+omopgenerics::exportCodelist(x = nsaids_codelist1, path = "1_InstantiateCohorts/Codelists/NSAIDs", type = "csv")
+omopgenerics::exportCodelist(x = ace_inhib, path = "1_InstantiateCohorts/Codelists", type = "csv")
+omopgenerics::exportConceptSetExpression(x = cough_codelist, path = "1_InstantiateCohorts/Codelists", type = "csv")
+
+write.csv(cough_codes, "1_InstantiateCohorts/Codelists/coughCodes.csv")
