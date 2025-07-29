67cb9e7fd40889d70596c7bac3973b66adb48724
daniellenewby <danielleanne.newby@gmail.com>
2025-07-29 15:26:54 +0100

    add in all nsaid characterisation

--- Study/2_Analysis/characterisation.R
+++ Study/2_Analysis/characterisation.R
@@ -140,6 +140,114 @@ if (isTRUE(run_characterisation)) {
       )
   })
   
+  
+  # all nsaids ------
+  # we characterise on all nsaids in one cohort
+  # bind to the settings and remove rows where marker is gi hem or stroke broad
+  # replace cohort_definition id with index_id (so it will run all nsaids together)
+  cdm$nsaids_characteristics1 <- cdm$nsaids_aesi %>%
+    left_join(
+      settings(cdm$nsaids_aesi) %>%
+        select(cohort_definition_id, index_id, index_name, marker_name),
+      by = "cohort_definition_id",
+      copy = TRUE
+    ) %>%
+    filter(!marker_name %in% c("gi_hemorrhage", "stroke_broad")) %>%
+    filter(!index_name %in% c("acetaminophen", "aspirin")) %>%
+    mutate(cohort_definition_id = index_id) %>%
+    PatientProfiles::addDemographics(
+      indexDate = "index_date",
+      ageGroup = list(
+        "age_group" = list(
+          "18 to 49" = c(18, 49),
+          "50 to 59" = c(50, 59),
+          "60 to 69" = c(60, 69),
+          "70 to 79" = c(70, 79),
+          "80 to 89" = c(80, 89),
+          "90+"     = c(90, 150)
+        )
+      )
+    ) %>%
+    distinct(subject_id, index_id, .keep_all = TRUE) %>%  
+    mutate(cohort_definition_id = 1, 
+           index_id = 1,
+           index_name = "all_nsaids") %>%
+    compute(name = "nsaids_characteristics1", temporary = FALSE)
+  
+  
+  # need to create new settings, attrition and codelist
+  # settings
+  new_settings <- cdm$nsaids_characteristics1 %>%
+    distinct(cohort_definition_id, index_id, index_name) %>%  # all together!
+    rename(cohort_name = index_name) %>%
+    collect() %>%  # collect locally if needed
+    tibble::as_tibble()
+  
+  # attrition
+  new_attrition <- tibble(
+    cohort_definition_id = integer(),
+    number_subjects = integer(),
+    number_records = integer(),
+    reason_id = integer(),
+    reason = character(),
+    excluded_subjects = integer(),
+    excluded_records = integer()
+  )
+  
+  # codelist ref
+  new_codelist_ref <- tibble(
+    cohort_definition_id = integer(),
+    codelist_name = character(),
+    concept_id = integer(),
+    codelist_type = character()
+  )
+  
+  
+  # add new settings, attrition and codelist ref to new cohort table
+  cdm$nsaids_characteristics1 <- omopgenerics::newCohortTable(
+    table = cdm$nsaids_characteristics1,
+    cohortSetRef = new_settings,
+    cohortAttritionRef = new_attrition,
+    cohortCodelistRef = new_codelist_ref,
+    .softValidation = TRUE
+    
+  )
+  
+  info(logger, "ADDED DEMOGRAPHICS TO COHORT")
+  
+  suppressWarnings({
+    summarycharacteristics_all <- cdm$nsaids_characteristics1 %>%
+      CohortCharacteristics::summariseCharacteristics(
+        ageGroup = list(
+          "18 to 49" = c(18, 49),
+          "50 to 59" = c(50, 59),
+          "60 to 69" = c(60, 69),
+          "70 to 79" = c(70, 79),
+          "80 to 89" = c(80, 89),
+          "90+" = c(90, 150)
+        ),
+        cohortIntersectFlag = list(
+          
+          "NSAID on index date" = list(
+            targetCohortTable = "nsaids",
+            window = c(0, 0)
+          ),
+          
+          "Conditions prior to index date" = list(
+            targetCohortTable = "conditions",
+            window = c(-Inf, -1)
+          ),
+          "Medications 365 days prior to index date" = list(
+            targetCohortTable = "medications",
+            window = c(-365, -1)
+          )
+        )
+      )
+  })
+  
+  
+  
+  
   cli::cli_alert_info("Exporting table one characteristics results ({Sys.time()})")
   info(logger, "EXPORTING TABLE ONE CHARACTERISTICS RESULTS")
   
@@ -149,6 +257,14 @@ if (isTRUE(run_characterisation)) {
     path = here::here(characterisation_folder),
     fileName = paste0(cdmName(cdm), "_summary_characteristics.csv")
   )
+  
+  omopgenerics::exportSummarisedResult(
+    summarycharacteristics_all,
+    minCellCount = 5,
+    path = here::here(characterisation_folder),
+    fileName = paste0(cdmName(cdm), "_summary_characteristics_all_nsaids.csv")
+  )
+  
   info(logger, "EXPORTED TABLE ONE CHARACTERISTICS RESULTS")
   
   cli::cli_alert_success("Table one Characterisation Analysis Complete ({Sys.time()})")
